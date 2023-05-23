d1c190dae0b0a7f3806e2d90b6dd1416e635d2c2
Nihat Eren Saltabas <122479699+saltabasnihat@users.noreply.github.com>
2023-05-23 00:15:51 -0400

    Add files via upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //enter first number
+
+            //repeat
+            //enter operator(+,-,*,/)
+            //enter next number
+            //until '='
+
+            //number 1(double)
+            //number 2(double)
+            //operator(string) 
+
+            double number1 = 0;
+            double number2 = 0;
+            string operation = null;
+
+            Console.Write("Enter the first number; ");
+            number1 = double.Parse(Console.ReadLine());
+
+            while (operation != "=")
+            {
+                Console.Write("Enter the operator: ");
+                operation = Console.ReadLine();
+
+                if (operation != "=")
+                {
+                    Console.Write("Enter the next number: ");
+                    number2 = double.Parse(Console.ReadLine());
+
+                    switch (operation)
+                    {
+                        case "+":
+                            number1 += number2;
+                            break;
+                        case "-":
+                            number1 -= number2;
+                            break;
+                        case "x":
+                        case "*":
+                            number1 *= number2;
+                            break;
+                        case "/":
+                            number1 /= number2;
+                            break;
+                        default:
+                            Console.WriteLine("Not a valid operator");
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine(number1);
+            Console.Read();
+        }
+    }
+}
