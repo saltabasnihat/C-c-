7072c95566007ef8e8a1766058b17640d6b17884
Nihat Eren Saltabas <122479699+saltabasnihat@users.noreply.github.com>
2023-05-22 23:10:38 -0400

    Add files via upload

---
+++ setup.1/Lab1/lab1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class lab1
+    {
+        static void Main(int[] args)
+        {
+            string name;
+
+            Console.Write("What is your name?");
+
+            name = int.Parse(Console.ReadLine());
+
+            Console.Write("Hello " + name + "\n");
+
+            Console.Write("Thank you for letting me learning C#. Press any key to exit program!");
+            Console.Read();
+        }
+    }
+}
