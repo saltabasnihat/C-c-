c207eacdf33cf85942b8149db8f5e8f053ecd3a5
Nihat Eren Saltabas <122479699+saltabasnihat@users.noreply.github.com>
2023-07-20 01:43:11 -0400

    Add files via upload

---
+++ Lab5_Statistic_Calculator/Calculator.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="Lab5_Statistic_Calculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Lab5</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+</head>
+<body>
+
+    <form runat="server" class="container mt-4">
+        <h1 class="text-center">Statistics Calculator</h1>
+        <div class="form-group">
+            <label for="txtFirst">First number:</label>
+            <asp:TextBox ID="txtFirst" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+
+        <div class="form-group">
+            <label for="txtSecond">Second number:</label>
+            <asp:TextBox ID="txtSecond" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+
+        <div class="form-group">
+            <label for="txtThird">Third number:</label>
+            <asp:TextBox ID="txtThird" runat="server" CssClass="form-control"></asp:TextBox>
+        </div>
+
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" CssClass="btn btn-primary" />
+    </form>
+    <div id="result" runat="server" class="container mt-4" visible="false">
+        <h3>Calculated Statistics</h3>
+        <p><strong>Maximum:</strong> <asp:Label ID="lblMax" runat="server"></asp:Label></p>
+        <p><strong>Minimum:</strong> <asp:Label ID="lblMin" runat="server"></asp:Label></p>
+        <p><strong>Average:</strong> <asp:Label ID="lblAverage" runat="server"></asp:Label></p>
+        <p><strong>Total:</strong> <asp:Label ID="lblTotal" runat="server"></asp:Label></p>
+    </div>
+</body>
+</html>
---
+++ Lab5_Statistic_Calculator/Calculator.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab5_Statistic_Calculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtFirst.Text, out double firstNumber) &&
+                double.TryParse(txtSecond.Text, out double secondNumber) &&
+                double.TryParse(txtThird.Text, out double thirdNumber))
+            {
+                //calculateation
+                double maximum = Math.Max(firstNumber, Math.Max(secondNumber, thirdNumber));
+                double minimum = Math.Min(firstNumber, Math.Min(secondNumber, thirdNumber));
+                double average = (firstNumber + secondNumber + thirdNumber) / 3;
+                double total = firstNumber + secondNumber + thirdNumber;
+
+                //display
+                lblMax.Text = maximum.ToString();
+                lblMin.Text = minimum.ToString();
+                lblAverage.Text = average.ToString();
+                lblTotal.Text = total.ToString();
+
+                result.Visible = true;
+            }
+            else
+            {
+                // Display an error message for invalid input
+                lblMax.Text = string.Empty;
+                lblMin.Text = string.Empty;
+                lblAverage.Text = string.Empty;
+                lblTotal.Text = string.Empty;
+
+                result.Visible = false;
+
+                ClientScript.RegisterStartupScript(this.GetType(), "showError", "alert('Input string was not in a correct format.');", true);
+            }
+        }
+    }
+}
